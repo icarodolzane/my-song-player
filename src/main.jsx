27dc98ec5b5250455460662ef4a727e0526fd9bb
icarodolzane <icarodolzane@live.com>
2023-03-15 18:55:12 -0400

    feat: Routes setup done.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Root from './routes/root';
+import Album from './pages/Album';
+// import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "/search",
+        element: <Search />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "/album/:id",
+        element: <Album />,
+    },
+    {
+        path: "/favorites",
+        element: <Favorites />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
+    },
+    {
+      path: "/profile/edit",
+      element: <ProfileEdit />,
+    }
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
